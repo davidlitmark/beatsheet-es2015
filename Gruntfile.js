51b111dc82e52bdf0f3aa837dd641abce8d9cebc
David Litmark <david@litmark.se>
2015-06-02 18:52:56 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+/*global module:false*/
+module.exports = function(grunt) {
+    "use strict";
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                node: true,
+                globals: {
+                    it: true,
+                    describe: true,
+                    chai: true,
+                    beatsheet: true
+                }
+            },
+            all: ['Gruntfile.js','js/*.js','test/spec/*.js']
+        },
+        uglify: {
+            options: {
+                preserveComments: 'some'
+            },
+            dist: {
+                files: {
+                    'dist/beatsheet.min.js' : 'js/beatsheet-es5.js'
+                }
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+        babel: {
+            dist: {
+                files: {
+                    'js/beatsheet-es5.js': 'js/beatsheet.js'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-babel');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'babel', 'uglify']);
+};
